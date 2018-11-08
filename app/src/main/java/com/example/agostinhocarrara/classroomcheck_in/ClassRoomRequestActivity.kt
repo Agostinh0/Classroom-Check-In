49fa153a3c520bd5c096596b106b6393111727fb
Matheus Cavalcanti <mthcavalcanti@gmail.com>
2018-11-08 01:09:21 -0300

    internacionalização e intents

--- app/src/main/java/com/example/agostinhocarrara/classroomcheck_in/ClassRoomRequestActivity.kt
+++ app/src/main/java/com/example/agostinhocarrara/classroomcheck_in/ClassRoomRequestActivity.kt
@@ -1,12 +1,19 @@
 package com.example.agostinhocarrara.classroomcheck_in
 
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_class_room_request.*
 
 class ClassRoomRequestActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_class_room_request)
+
+        val intent: Intent = getIntent()
+        val laboratorio: String = intent.getStringExtra("lab")
+
+        tvClassroom.setText(laboratorio)
     }
 }
--- app/src/main/java/com/example/agostinhocarrara/classroomcheck_in/MainActivity.kt
+++ app/src/main/java/com/example/agostinhocarrara/classroomcheck_in/MainActivity.kt
@@ -1,12 +1,44 @@
 package com.example.agostinhocarrara.classroomcheck_in
 
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        btn35.setOnClickListener{
+            val intent = Intent(this, ClassRoomRequestActivity::class.java)
+            intent.putExtra("lab", "Lab. 35")
+            startActivityForResult(intent, 1)
+        }
+
+        btn37.setOnClickListener{
+            val intent = Intent(this, ClassRoomRequestActivity::class.java)
+            intent.putExtra("lab", "Lab. 37")
+            startActivityForResult(intent, 1)
+        }
+
+        btn39.setOnClickListener{
+            val intent = Intent(this, ClassRoomRequestActivity::class.java)
+            intent.putExtra("lab", "Lab. 39")
+            startActivityForResult(intent, 1)
+        }
+
+        btn41.setOnClickListener{
+            val intent = Intent(this, ClassRoomRequestActivity::class.java)
+            intent.putExtra("lab", "Lab. 41")
+            startActivityForResult(intent, 1)
+        }
+
+        btn43.setOnClickListener{
+            val intent = Intent(this, ClassRoomRequestActivity::class.java)
+            intent.putExtra("lab", "Lab. 43")
+            startActivityForResult(intent, 1)
+        }
     }
 }
